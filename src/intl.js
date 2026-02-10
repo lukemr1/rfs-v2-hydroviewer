5dbf22a40488cbe9724b9fa2089ba30264134ef3
rhuber6 <94189614+rhuber6@users.noreply.github.com>
2026-02-10 11:20:12 -0700

    translated to french (#15)

--- src/intl.js
+++ src/intl.js
@@ -1,5 +1,6 @@
 import en from './json/locales/en-US.json' with {type: "json"}
 import es from './json/locales/es.json' with {type: "json"}
+import fr from './json/locales/fr.json' with {type: "json"} 
 
 // get the locale based on the path in the url given by the pattern /{lang}/...
 const lang = window.location.pathname.split("/").filter(x => x && !x.includes(".html") && !x.includes('viewer'))[0] || 'en'
@@ -12,6 +13,9 @@ switch (lang) {
     break
   case 'es':
     text = es
+    break
+  case 'fr':
+    text = fr
     break
   default:
     text = en
